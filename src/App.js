bd388f42f473fe2ec1a94420d94b82d258f1edce
Ileana Ordonez <ileana.s.ordonez@gmail.com>
2017-12-28 14:07:22 -0500

    public folder with images/css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
+
 import Nav from './components/Nav.jsx';
 import Projects from './components/Projects.jsx';
 
@@ -9,20 +9,8 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Particles
-              params={{
-            		particles: {
-            			line_linked: {
-            				shadow: {
-            					enable: true,
-            					color: "#3CA9D1",
-            					blur: 1
-            				}
-            			}
-            		}
-            	}}
-            />
           <Nav />
+          <Projects />
         </div>
     )
   }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 
 const Nav = () => {
   return(
     <div id="me">
       <h1>Ileana Ordonez</h1>
-
+        <Particles
+                params={{
+              		particles: {
+              			line_linked: {
+              				shadow: {
+              					enable: true,
+              					color: "#3CA9D1",
+              					blur: 1
+              				}
+              			}
+              		}
+              	}}
+              />
     </div>
   )
 }
